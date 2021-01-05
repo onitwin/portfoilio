6a976f688592d0dcf2caf952ed457a0b2b9ef6bc
onitwin <onitwin@gmail.com>
2021-01-05 16:56:13 +0000

    added carosel styling

--- src/carousel/Circuit.js
+++ src/carousel/Circuit.js
@@ -16,26 +16,28 @@ const Circuit=()=>{
     <div className="carousel-container">
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
+      <div className="carousel-internal-block">
         <img
           className="carousel-image"
-          src="Images/me.jpeg"
+          src="Images/scroller.jpeg"
           alt="First slide"
         />
         <Carousel.Caption>
           <h3>1</h3>
-          <p>First SLide Text</p>
+          <p className="carousel-text">Scroller</p>
         </Carousel.Caption>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="carousel-image"
-          src="Images/me.jpeg"
+          className="carousel-image spaceX"
+          src="Images/SpaceX.jpeg"
           alt="Second slide"
         />
 
         <Carousel.Caption>
           <h3>2</h3>
-          <p>Second Slide Text</p>
+          <p className="carousel-text">SpaceX Launch Tracker</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -47,26 +49,18 @@ const Circuit=()=>{
 
         <Carousel.Caption>
           <h3>3</h3>
-          <p>
-            Third Slide Text
+          <p className="carousel-text">
+            ISS Tracker
           </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carousel-image"
-          src="Images/me.jpeg"
-          alt="Third slide"
-        />
 
-        <Carousel.Caption>
-          <h3>4</h3>
-          <p>
-            Forth Slide Text
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
     </Carousel>
+    </div>
+    <div className="carousel-descriptor-container">
+    <div className="carousel-descriptor" >
+    <h2>These are some of the projects I have been working on recently</h2>
+    </div>
     </div>
     </div>
   )

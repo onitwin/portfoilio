6599104502aa13bd84f1bf69b6b83bb09e5ce2c4
onitwin <onitwin@gmail.com>
2021-01-06 11:15:24 +0000

    added map function to carousel to maintain DRY priciples

--- src/carousel/Circuit.js
+++ src/carousel/Circuit.js
@@ -1,10 +1,29 @@
-import React,{useState} from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import React,{useState} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import items from "../data";
 
 
 
 const Circuit=()=>{
+
+  const projectsMap=items.map((item,key)=>{
+    return(
+      <Carousel.Item key={item.id}>
+      <div className="carousel-internal-block">
+        <img
+          className={item.className}
+          src={item.image}
+          alt={item.projectName}
+        />
+        <Carousel.Caption>
+          <h3>{item.projectName}</h3>
+          <p className="carousel-text">{item.projectName}</p>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+    )
+  })
+
   const [index, setIndex] = useState(0);
 
   const handleSelect=(selectedIndex,e)=>{
@@ -16,46 +35,7 @@ const Circuit=()=>{
     <div className="circuit-container">
     <div className="carousel-container">
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-      <div className="carousel-internal-block">
-        <img
-          className="carousel-image"
-          src="Images/scroller.jpeg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>1</h3>
-          <p className="carousel-text">Scroller</p>
-        </Carousel.Caption>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carousel-image spaceX"
-          src="Images/SpaceX.jpeg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>2</h3>
-          <p className="carousel-text">SpaceX Launch Tracker</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carousel-image"
-          src="Images/me.jpeg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>3</h3>
-          <p className="carousel-text">
-            ISS Tracker
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
+    {projectsMap}
     </Carousel>
     </div>
     <div className="carousel-descriptor-container">
@@ -68,3 +48,44 @@ const Circuit=()=>{
 }
 
 export default Circuit;
+
+
+// <Carousel.Item>
+// <div className="carousel-internal-block">
+//   <img
+//     className="carousel-image"
+//     src="Images/scroller.jpeg"
+//     alt="First slide"
+//   />
+//   <Carousel.Caption>
+//     <h3>1</h3>
+//     <p className="carousel-text">Scroller</p>
+//   </Carousel.Caption>
+//   </div>
+// </Carousel.Item>
+// <Carousel.Item>
+//   <img
+//     className="carousel-image large"
+//     src="Images/SpaceX.jpeg"
+//     alt="Second slide"
+//   />
+//
+//   <Carousel.Caption>
+//     <h3>2</h3>
+//     <p className="carousel-text">SpaceX Launch Tracker</p>
+//   </Carousel.Caption>
+// </Carousel.Item>
+// <Carousel.Item>
+//   <img
+//     className="carousel-image"
+//     src="Images/me.jpeg"
+//     alt="Third slide"
+//   />
+//
+//   <Carousel.Caption>
+//     <h3>3</h3>
+//     <p className="carousel-text">
+//       ISS Tracker
+//     </p>
+//   </Carousel.Caption>
+// </Carousel.Item>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,16 @@
 export default[
-  {id:1,projectName:"Scroller",description:"Simple app to allow the user to enter text and then display it as scrolling text when they rotate their screen",image:"Images/scroller.jpeg"},
-  {id:2,projectName:"SpaceX Launcher",description:"Fetches data from SpaceX api and displays the details about the launch. Incorporates filter and sort functions",image:"Images/spacex.jpeg"},
-{id:3,projectName:"ISS Tracker",description:"Fetches positional information for the International Space Station and displays a map of its path in real time",image:"Images/iss.jpeg"}]
+  {id:1,
+    projectName:"Scroller",
+  description:"Simple app to allow the user to enter text and then display it as scrolling text when they rotate their screen",
+  image:"Images/scroller.jpeg",
+  className:"carousel-image"},
+  {id:2,
+    projectName:"SpaceX Launcher",
+    description:"Fetches data from SpaceX api and displays the details about the launch. Incorporates filter and sort functions",
+    image:"Images/spacex.jpeg",
+    className:"carousel-image large"},
+{id:3,
+  projectName:"ISS Tracker",
+  description:"Fetches positional information for the International Space Station and displays a map of its path in real time",
+  image:"Images/iss.jpeg",
+  className:"carousel-image"}]

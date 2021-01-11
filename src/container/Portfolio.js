7aa3d28e0ebca7622f5ea54892f642178a76a4e5
onitwin <onitwin@gmail.com>
2021-01-11 16:02:24 +0000

    updated project component

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Project from "../project/Project"
 
 
 const Portfolio=()=>{
 
 
   return(
-    <p>hello from the PortFolio page</p>
+    <div className="portfolio-container">
+    <div className="portfolio-header-wrapper">
+    <h1 className="portfolio-header">Project Portfolio</h1>
+    </div>
+    <div className="portfolio-description-wrapper">
+    <h2 className="portfolio-description">Below are a few of my most recent or favourite projects</h2>
+    </div>
+    <Project/>
+    </div>
   )
 }
 

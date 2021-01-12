449e6fa260ee0f1042c9ac6de365351e354f7bab
onitwin <onitwin@gmail.com>
2021-01-12 16:49:16 +0000

    portfolio structure complete

--- src/project/Project.js
+++ src/project/Project.js
@@ -9,20 +9,29 @@ const Project=({item})=>{
 
   return(
     <div className="project-container-inner">
+
     <div className="project-details-container">
 
     <div className="project-image-container">
     <div className="title-container">
-    <h3 className="project-title">{item.projectName}</h3>
+    <a href={item.link}><h3 className="project-title">{item.projectName}</h3></a>
     </div>
     <div className="project-image-inner">
     <img className={item.className} src={item.image}/>
     </div>
     </div>
     <div className="project-description-container">
-    <article><p>text goes here </p></article>
+    <article><p>{item.detailedDescription} </p></article>
     <div className="tech-stack-container">
-    <h2>Tech Stack: icons for tech goes here;</h2>
+    <div className="tech-stack-descriptor">
+    <div className="tech-stack-textbox">
+    <h2>Tech Stack</h2>
+    </div>
+    <div className="tech-stack-icons-container">
+    <h2>hello</h2>
+    <h2>Hi</h2>
+    </div>
+    </div>
     </div>
     </div>
 

964faeaf2fa34ddce865d7de83374e9c7dee8875
onitwin <onitwin@gmail.com>
2021-01-14 11:42:49 +0000

    corrected allignment issue in phone view

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -11,11 +11,8 @@ const Portfolio=({items})=>{
     <div className="portfolio-header-wrapper">
     <h1 className="portfolio-header">Project Portfolio</h1>
     </div>
-    <div className="portfolio-description-wrapper">
     <h2 className="portfolio-description">Below are a few of my most recent or favourite projects</h2>
-    </div>
     <Projects items={items}/>
-
     </div>
   )
 }

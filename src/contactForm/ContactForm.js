64a1048797fb3e067902c9fdd1229f96e9cc6968
onitwin <onitwin@gmail.com>
2021-02-23 16:25:46 +0000

    form created- inactive

--- src/contactForm/ContactForm.js
+++ src/contactForm/ContactForm.js
@@ -8,9 +8,25 @@ const ContactForm=()=>{
   return(
     <div className="contact-form-wrapper">
     <form className="formDiv">
+
     <label for="name">Your name</label>
-    <input type="text" id="name">
+    <input type="text" id="name" required>
     </input>
+    <br/>
+
+    <label for="subject">Subject</label>
+    <input type="text" id="subject" required>
+    </input>
+    <br/>
+
+    <label for="email">Your email</label>
+    <input type="text" id="email" required>
+    </input>
+    <br/>
+
+    <label for="details">Your message</label>
+    <textarea  id="details" required >
+    </textarea>
     </form>
 
     </div>

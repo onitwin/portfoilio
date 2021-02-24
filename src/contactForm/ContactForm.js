8e0024d5630474a3560b104d7f7a12c43afba079
onitwin <onitwin@gmail.com>
2021-02-24 09:45:58 +0000

    added contact form

--- src/contactForm/ContactForm.js
+++ src/contactForm/ContactForm.js
@@ -7,26 +7,29 @@ const ContactForm=()=>{
 
   return(
     <div className="contact-form-wrapper">
-    <form className="formDiv">
+    <form className="formDiv"  action="https://formspree.io/f/xnqokadb" method="POST">
 
     <label for="name">Your name</label>
-    <input type="text" id="name" required>
+    <input type="text" id="name" required name="name">
     </input>
     <br/>
 
     <label for="subject">Subject</label>
-    <input type="text" id="subject" required>
+    <input type="text" id="subject" required name="subject">
     </input>
     <br/>
 
     <label for="email">Your email</label>
-    <input type="text" id="email" required>
+    <input type="text" id="email" required name="email">
     </input>
     <br/>
 
     <label for="details">Your message</label>
-    <textarea  id="details" required >
+    <textarea  id="details" required name="message">
     </textarea>
+    <br/>
+    <button className="formSubmit" type="submit">Send Message</button>
+
     </form>
 
     </div>

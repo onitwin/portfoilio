7d937a0db8afb3c289d689d4c6cf44e2739e10ca
onitwin <onitwin@gmail.com>
2021-02-24 09:55:40 +0000

    added linkedin link

--- src/container/Contact.js
+++ src/container/Contact.js
@@ -10,7 +10,7 @@ const Contact=()=>{
     <div className="contact-header-wrapper">
     <h1 className="contact-page-header">Contact Me</h1>
     </div>
-    <h2 className="contact-description">Please contact me using this form or via LinkedIn</h2>
+    <h2 className="contact-description">Please contact me using this form or via <a href="https://linkedin.com/in/joseph-sutherland81/">LinkedIn</a></h2>
     <ContactForm/>
     </div>
   )

f33d373d7257e273e792ecc63ad3c7c3158e833d
Joseph Sutherland <57771768+onitwin@users.noreply.github.com>
2021-10-20 16:27:26 +0100

    Update IntroText.js

--- src/intro/IntroText.js
+++ src/intro/IntroText.js
@@ -6,7 +6,7 @@ const IntroText=()=>{
 
   return(
     <div className="intro-text-container">
-    <h2 className="intro-text">A fresh out-the-packet software developer looking for their first role.
+    <h2 className="intro-text">A fresh out-the-packet software developer.
     Constantly looking for opportunities to learn. Have experience with Java, Javascript, React and Vue.
     I love the responsive nature of programming and am excited for the future.</h2>
     </div>
